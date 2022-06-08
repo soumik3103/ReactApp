0ad760b26f5f16fd63f1eeff50fd7e6917d5e936
soumik3103 <107119208+soumik3103@users.noreply.github.com>
2022-06-08 22:40:30 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+//This is my first javascript code
+let person = {
+    firstName: 'Soumik',
+    age: 27
+}
+let selection = 'age';
+person.age = 30;
+let soumikAge = [10,20];
+console.log(soumikAge[0]);
+console.log(person[selection]);
+
+function greet(lastName){
+    console.log("Hello "+lastName)
+}
+
+greet('Mukherjee');
+
